bd94078c77e2d677e5ef7d0fd22cbe2511ae257a
Richard Stöök <ricsth91@gmail.com>
2023-10-18 14:25:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ window.addEventListener("scroll", function () {
 
   lastScrollPosition = currentScrollPosition;
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const fullScreenMenu = document.querySelector(".full-screen-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  fullScreenMenu.style.display = navMenu.classList.contains("active")
+    ? "block"
+    : "none";
+});
+
+const menuLinks = document.querySelectorAll(".nav-menu a");
+
+function closeMenu() {
+  const menu = document.querySelector(".nav-menu");
+  menu.classList.remove("active");
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+const hamburgerIcon = document.querySelector(".hamburger");
+
+function closeMenuAndResetHamburger() {
+  const menu = document.querySelector(".nav-menu");
+  menu.classList.remove("active");
+  hamburgerIcon.classList.remove("active");
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenuAndResetHamburger);
+});
